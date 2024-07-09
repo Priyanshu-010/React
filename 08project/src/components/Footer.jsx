359a9e85a88349d73aee74f45ebcaffc76576c01
Priyanshu-010 <priyanshurai2772@gmail.com>
2024-07-09 17:00:33 +0530

    08 project completed

---
+++ 08project/src/components/Footer.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>Priyanshu Rai</h1>
+        <p>@all right reserved</p>
+      </div>
+
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+          <a href="https://x.com/Priyanshu10101" target={"blank"}>
+            X
+          </a>
+          <a href="https://instagram.com/priyanshu.010" target={"blank"}>
+            Instagram
+          </a>
+          <a href="https://github.com/Priyanshu-010" target={"blank"}>
+            Github
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;  
